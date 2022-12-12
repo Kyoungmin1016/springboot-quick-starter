da099bf5544a612af2cabaf13ca0f877c29d890f
Kyongmin1016 <rkdrnwkd1@gmail.com>
2022-12-13 00:51:10 +0900

    3-2

--- Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.rubypaper.controller;
 
 import com.rubypaper.domain.BoardVO;
+import com.rubypaper.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,24 +12,26 @@ import java.util.List;
 
 @RestController
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
     public BoardController(){
         System.out.println("===> BoardController 생성");
     }
 
     @GetMapping("/getBoardList")
     public List<BoardVO> getBoardList(){
-        List<BoardVO> boardList = new ArrayList<BoardVO>();
-        for (int i = 1; i<=10;i++){
-            BoardVO board = new BoardVO();
-            board.setSeq(i);
-            board.setTitle("제목"+i);
-            board.setWriter("테스터");
-            board.setContent(i+"번 내용입니다.");
-            board.setCreateDate(new Date());
-            board.setCnt(0);
-            boardList.add(board);
-        }
+        return boardService.getBoardList();
+    }
 
-        return boardList;
+    @GetMapping("/hello")
+    public String hello(String name){
+        return boardService.hello(name);
+    }
+
+    @GetMapping("/getBoard")
+    public BoardVO getBoard(){
+        return boardService.getBoard();
     }
 }
